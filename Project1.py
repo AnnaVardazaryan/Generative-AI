aa76edf6fd4645023e65f912b6cdcb731344f635
Anna Vardazaryan <101353656+AnnaVardazaryan@users.noreply.github.com>
2023-07-25 11:55:22 +0400

    Add files via upload

--- Project1.py
+++ Project1.py
@@ -1,173 +1,186 @@
-""" You are writing an inventory application for a budding tech guy who has a video channel featuring computer builds. Basically they have a pool of inventory, (for example 5 x AMD Ryzen 2-2700 CPUs) that they use for builds. When they take a CPU from the pool. They will indicate this using the object that tracks that specific type of CPU. They may also purchase additional CPUs, or retire some (because they overclocked it too much and burnt them out).
-Technically we would want a database to back all this data, but here we’re just going to build classes we’ll use while our program is running and not worry about retrieving or saving the state of the inventory.
-The base class is going to be a general Resource. This class should provide functionality common to all the actual resources (CPU, GPU, Memory, HDD, SSD) - for the exercise we’re only going to implement CPU, HDD and SSD.
+"""In this programming exercise, you are provided with a boilerplate code for defining two classes, Point2D and Point3D,
+representing points in a 2-dimensional and 3-dimensional space, respectively. The provided code uses special methods like
+__init__, __eq__, __hash__, and properties to encapsulate the data and provide certain functionalities. However, the code is not generic,
+and it involves some repetition.
 
-Input and Output examples
-It should provide this at minimum:
- name : user-friendly name of resource instance(e.g. Intel Core i9-9900K)
- manufacturer : resource instance manufacturer(e.g. Nvidia)
- total : inventory total(how many are in the inventory pool)
- allocated : number allocated(how many already in use)
- str representation that just returns the resource name
- a mode detailed repr implementation
- claim(m) : method to take n resources from the pool(as long as inventory os available)
- freeup(n) : method to return n resources to the pool(e.g disassembled some builds)
- died(n) : method to return and permanently remove inventory from the pool(e.g. They broke something) - as long as total available allows it
- purchased(n) : method to add inventory to the pool(e.g. They purchased a new CPU)
- Category - computed property that returns a lowercase version of the class name
+Task is to refactor the provided code to make it more generic and DRY (Don’t Repeat Yourself) by using metaclasses.
+Metaclasses allow you to create classes dynamically, enabling you to define common behaviors for multiple classes in an
+elegant and efficient manner. Your refactored code should be able to generate N-dimensional point classes with the same
+functionalities while minimizing duplication of code.
 
 Hints
- For the CPU class:
- cores (e.g. 8)The SSD class extends Storage and has these additional properties:
- interface (e.g. PCIe NMVe 3.0 x4)
- socket (e.g. AM4)
- power_watts (e.g. 94)
- For the HDD and SSD classes, we’re going to create an intermediate class called Storage with these additional properties:
- capacity_GB (e.g. 120)
- The HDD class extends Storage and has these additional properties:
- size (e.g. 2.5")
- rpm (e.g. 7000)
- The SSD class extends Storage and has these additional properties:
- interface (e.g. PCIe NMVe 3.0 x4)
- For all your classes, implement a full constructor that can be used to initialize all the properties, some form of validation on numeric types, as well customized repr as you see fit.
- For the total and allocated values in the Resource init, think of the arguments there as the current total and allocated counts. Those total and allocated attributes should be private read-only properties, but they are modifiable through the various methods such as claim, return, died, purchased. Other attributes like name, manufacturer_name, etc should be read-only."""
+Analyze the provided Point2D and Point3D classes and identify the common functionalities that can be generalized for
+N-dimensional points.
+Create a custom metaclass called SlottedStruct that defines these common functionalities, such as initializing the
+point’s coordinates, comparing points for equality, computing the hash, and generating string representations.
+Modify the Point2D and Point3D classes to use the SlottedStruct metaclass instead of the current implementations.
+Make sure the classes still function correctly after the changes.
+Test your refactored code with various N-dimensional points (e.g., Point4D, Point5D) to verify that the generic
+implementation works as expected.
+"""
 
+class Point2D:
+    """Class representing a point in 2-dimensional space."""
 
-class Resource:
-    def __init__(self, name, manufacturer, total, allocated):
-        self._name = name
-        self._manufacturer = manufacturer
-        self._total = total
-        self._allocated = allocated
+    def __init__(self, x, y):
+        """Initialize a Point2D object with x and y coordinates."""
+        self._x = x
+        self._y = y
 
     @property
-    def name(self):
-        return self._name
+    def x(self):
+        """Get the x-coordinate of the point."""
+        return self._x
 
     @property
-    def manufacturer(self):
-        return self._manufacturer
+    def y(self):
+        """Get the y-coordinate of the point."""
+        return self._y
 
-    @property
-    def total(self):
-        return self._total
+    def __eq__(self, other):
+        """Check if two Point2D objects are equal based on their coordinates."""
+        return isinstance(other, Point2D) and (self.x, self.y) == (other.x, other.y)
 
-    @property
-    def allocated(self):
-        return self._allocated
+    def __hash__(self):
+        """Compute the hash value of the Point2D object based on its coordinates."""
+        return hash((self.x, self.y))
+
+    def __repr__(self):
+        """Get the string representation of the Point2D object."""
+        return f'Point2D({self.x}, {self.y})'
 
     def __str__(self):
-        return self._name
+        """Get the user-friendly string representation of the Point2D object."""
+        return f'({self.x}, {self.y})'
+
+class Point3D:
+    """Class representing a point in 3-dimensional space."""
+
+    def __init__(self, x, y, z):
+        """Initialize a Point3D object with x, y, and z coordinates."""
+        self._x = x
+        self._y = y
+        self._z = z
+
+    @property
+    def x(self):
+        """Get the x-coordinate of the point."""
+        return self._x
+
+    @property
+    def y(self):
+        """Get the y-coordinate of the point."""
+        return self._y
+
+    @property
+    def z(self):
+        """Get the z-coordinate of the point."""
+        return self._z
+
+    def __eq__(self, other):
+        """Check if two Point3D objects are equal based on their coordinates."""
+        return isinstance(other, Point3D) and (self.x, self.y, self.z) == (other.x, other.y, other.z)
+
+    def __hash__(self):
+        """Compute the hash value of the Point3D object based on its coordinates."""
+        return hash((self.x, self.y, self.z))
 
     def __repr__(self):
-        return f"Resource(name={self._name}, manufacturer={self._manufacturer}, total={self._total}, allocated={self._allocated})"
+        """Get the string representation of the Point3D object."""
+        return f'Point3D({self.x}, {self.y}, {self.z})'
 
-    def claim(self, n):
-        if self._total - self._allocated >= n:
-            self._allocated += n
-        else:
-            return "Not enough resources"
+    def __str__(self):
+        """Get the user-friendly string representation of the Point3D object."""
+        return f'({self.x}, {self.y}, {self.z})'
 
-    def freeup(self, n):
-        if self._allocated >= n:
-            self._allocated -= n
-        else:
-            return "Not enough resources"
-
-    def died(self, n):
-        if self._total >= n:
-            self._total -= n
-        else:
-            return "Not enough resources"
-
-    def purchased(self, n):
-        self._total += n
-
-    @property
-    def category(self):
-        return self.__class__.__name__.lower()
-
-class Storage(Resource):
-    def __init__(self, name, manufacturer, total, allocated, capacity_GB):
-        super().__init__(name, manufacturer, total, allocated)
-        self._capacity_GB = capacity_GB
-
-    @property
-    def capacity_GB(self):
-        return self._capacity_GB
-
-    def __repr__(self):
-        return f"Storage(name={self._name}, manufacturer={self._manufacturer}, total={self._total}, allocated={self._allocated}, capacity_GB={self._capacity_GB})"
-
-class HDD(Storage):
-    def __init__(self, name, manufacturer, total, allocated, capacity_GB, size, rpm):
-        super().__init__(name, manufacturer, total, allocated, capacity_GB)
-        self._size = size
-        self._rpm = rpm
-
-    @property
-    def size(self):
-        return self._size
-
-    @property
-    def rpm(self):
-        return self._rpm
-
-    def __repr__(self):
-        return f"HDD(name={self._name}, manufacturer={self._manufacturer}, total={self._total}, allocated={self._allocated}, capacity_GB={self._capacity_GB}, size={self._size}, rpm={self._rpm})"
-
-class SSD(Storage):
-    def __init__(self, name, manufacturer, total, allocated, capacity_GB, interface):
-        super().__init__(name, manufacturer, total, allocated, capacity_GB)
-        self._interface = interface
-
-    @property
-    def interface(self):
-        return self._interface
-
-    def __repr__(self):
-        return f"SSD(name={self._name}, manufacturer={self._manufacturer}, total={self._total}, allocated={self._allocated}, capacity_GB={self._capacity_GB}, interface={self._interface})"
-
-class CPU(Resource):
-    def __init__(self, name, manufacturer, total, allocated, cores, interface, socket, power_watts):
-        super().__init__(name, manufacturer, total, allocated)
-        self._cores = cores
-        self._interface = interface
-        self._socket = socket
-        self._power_watts = power_watts
-
-    @property
-    def cores(self):
-        return self._cores
-
-    def __repr__(self):
-        return f"CPU(name={self._name}, manufacturer={self._manufacturer}, total={self._total}, allocated={self._allocated}, cores={self._cores})"
-
-# Testing the classes
-cpu = CPU("Intel Core i9-9900K", "Intel", 10, 5, 8, "PCIe NMVe 3.0 x4", "AM4", 94)
-print(cpu)      # Intel Core i9-9900K
-print(cpu.category)     # cpu
-print(cpu.cores)        # 8
-print(cpu.total)        # 10
-print(cpu.allocated)  # 5
-print(cpu.claim(2))     # None
-print(cpu.freeup(1))        # None
-print(cpu.died(1))      # None
-print(cpu.purchased(1))     # None
-print(cpu.total)        # 10
-print(cpu.allocated)        # 6
-
-hdd = HDD("Seagate BarraCuda", "Seagate", 10, 5, 120, "2.5\"", 7000)
-print(hdd)      # Seagate BarraCuda
-print(hdd.category)     # hdd
-print(hdd.size)     # 2.5"
-print(hdd.rpm)      # 7000
-print(hdd.capacity_GB)      # 120
+# In the above implementation, we have separate classes (Point2D and Point3D) for different dimensions, leading to code repetition.
+# Writing the same code multiple times for different dimension classes is inefficient and makes maintenance challenging.
+# Let's adopt a smarter strategy to eliminate redundancy and improve code maintainability.
 
 
-ssd = SSD("Samsung 970 EVO", "Samsung", 10, 5, 120, "PCIe NMVe 3.0 x4")
-print(ssd)      # Samsung 970 EVO
-print(ssd.category)     # ssd
-print(ssd.interface)        # PCIe NMVe 3.0 x4
-print(ssd.capacity_GB)      # 120
+# DRY Version - Using Metaclasses
+class SlottedStruct(type):
+    """Metaclass to dynamically create classes for N-dimensional points."""
 
+    def __new__(cls, name, bases, namespace):
+        """Create a new class dynamically based on its name and dimension."""
+        if name != 'Point':
+            dim = int(name.split('D')[0][-1])  # Extract dimension from the class name
+            namespace['__slots__'] = tuple(f'_{i}' for i in range(1, dim + 1))
+            namespace['__init__'] = cls._init_coordinates
+            namespace['__eq__'] = cls._eq_coordinates
+            namespace['__hash__'] = cls._hash_coordinates
+            namespace['__repr__'] = cls._repr_coordinates
+            namespace['__str__'] = cls._str_coordinates
+
+        return super().__new__(cls, name, bases, namespace)
+
+    def _init_coordinates(self, *args):
+        """Initialize the N-dimensional point object with coordinates."""
+        for i, arg in enumerate(args, start=1):
+            setattr(self, f'_{i}', arg)
+
+    def _eq_coordinates(self, other):
+        """Check if two N-dimensional point objects are equal based on their coordinates."""
+        return isinstance(other, Point) and tuple(getattr(self, f'_{i}') for i in range(1, len(self.__slots__) + 1)) == \
+               tuple(getattr(other, f'_{i}') for i in range(1, len(self.__slots__) + 1))
+
+    def _hash_coordinates(self):
+        """Compute the hash value of the N-dimensional point object based on its coordinates."""
+        return hash(tuple(getattr(self, f'_{i}') for i in range(1, len(self.__slots__) + 1)))
+
+    def _repr_coordinates(self):
+        """Get the string representation of the N-dimensional point object."""
+        dim = len(self.__slots__)
+        coord_str = ', '.join(str(getattr(self, f'_{i}')) for i in range(1, dim + 1))
+        return f'Point{dim}D({coord_str})'
+
+    def _str_coordinates(self):
+        """Get the user-friendly string representation of the N-dimensional point object."""
+        dim = len(self.__slots__)
+        coord_str = ', '.join(str(getattr(self, f'_{i}')) for i in range(1, dim + 1))
+        return f'({coord_str})'
+
+
+class Point(metaclass=SlottedStruct):
+    """Base class for N-dimensional points."""
+
+    pass
+class Point2D(Point):
+    """Class representing a point in 2-dimensional space."""
+
+    pass
+
+
+class Point3D(Point):
+    """Class representing a point in 3-dimensional space."""
+
+    pass
+
+
+class Point4D(Point):
+    """Class representing a point in 4-dimensional space."""
+
+    pass
+
+class Point5D(Point):
+    """Class representing a point in 5-dimensional space."""
+
+    pass
+
+# Here the metaclass SlottedStruct dynamically creates classes for N-dimensional points.
+# It is responsible for creating the __slots__, __init__, __eq__, __hash__, __repr__, and __str__ methods for the
+# N-dimensional point classes and also for creating the __new__ method for the N-dimensional point classes.
+
+
+# Test
+
+p1 = Point2D(1, 2)
+p2 = Point3D(1, 2, 3)
+p3 = Point4D(1, 2, 3, 4)
+p4 = Point5D(1, 2, 3, 4, 5)
+
+print(p1)  # Output: (1, 2)
+print(p2)  # Output: (1, 2, 3)
+print(p3)  # Output: (1, 2, 3, 4)
+print(p4)  # Output: (1, 2, 3, 4, 5)
